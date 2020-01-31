946e116e0d767e4f6d0fc8c4b0d925a1ca4dbcd9
Alen-Issac-Cherian <60470499+Alen-Issac-Cherian@users.noreply.github.com>
2020-02-01 00:49:19 +0530

    Add files via upload

---
+++ project tesla.py
@@ -0,0 +1,19 @@
+from vpython import *
+import serial
+
+window = canvas(title="Output Screen")
+window.width = 700
+window.height = 600
+
+C = curve(color=color.red, radius=0.1)
+arduino = serial.Serial('com7', 9600)
+
+while 1:
+    rate(30)
+    if arduino.inWaiting() > 0:
+        data = arduino.readline()
+        variables = data.split(' ')
+        r = float(variables[0])
+        angle = int(variables[1])
+        V = vector((r*cos(angle)), (r*sin(angle)), 0)
+        C.append(V)
